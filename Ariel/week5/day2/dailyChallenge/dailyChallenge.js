38cd9ae411aabd2e034a8d0bb12e6bffd873e708
ShadowFrost3167 <olivertwist3167@gmail.com>
2024-03-25 14:05:01 +0200

    dailyChallengeDay2

--- Ariel/week5/day2/dailyChallenge/dailyChallenge.js
+++ Ariel/week5/day2/dailyChallenge/dailyChallenge.js
@@ -1,13 +1,53 @@
+// =======================================================================
+//                       DAILY CHALLENGE 1
+
+
+
 // Instructions
 // Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
 
-var sentence = "not"+"bad"
+var sentence = "Hollow Knight is not too bad!"
 // For example, “The movie is not that bad, I like it”.
 
 // Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
+var indexStart = sentence.indexOf("not");
+console.log(indexStart)
+
+var indexEnd = sentence.indexOf("!");
+console.log(indexEnd)
+
+let notTooBad = sentence.substring(indexStart, indexEnd)
 
 // Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
-
+var wordBad = sentence.indexOf("bad");
 // If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+if (indexStart < wordBad){
+    var good = "good"
+   sentence =  sentence.replace("not too bad", good);
+   console.log(sentence);
+} else {console.log(sentence)}
+
+
 // For example, the result here should be : “The movie is good, I like it”
 // If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+
+//==========================================================================
+//                          DAILY CHALLENGE 2
+
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+let star = ["*"]
+
+for (i = 0; i <= 5; i++) {
+    console.log(star)
+    star.push("*");
+    
+} 
